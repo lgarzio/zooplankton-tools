5884762ae50dd1cd56bf41e5aab53a0a2f751f06
lgarzio <lgarzio@marine.rutgers.edu>
2020-01-31 12:34:08 -0500

    script to calculate experiment time in hours

---
+++ data_analysis/calculate_expt_time.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+"""
+Created on Jan 30 2020 by Lori Garzio
+@brief Calculate experiment time in hours and add to the input csv file as a column
+"""
+
+import datetime as dt
+import pandas as pd
+pd.set_option('display.width', 320, "display.max_columns", 10)  # for display in pycharm console
+
+csv_file = '/Users/lgarzio/Documents/rucool/Saba/microplastics/NOAA2018/data/DEBay_MP_expt2.csv'
+
+df = pd.read_csv(csv_file)
+
+df['t0mod'] = df['t0'].map(lambda t: dt.datetime.strptime(t, '%Y-%m-%dT%H:%M'))
+df['tfmod'] = df['tf'].map(lambda t: dt.datetime.strptime(t, '%Y-%m-%dT%H:%M'))
+df['expt_time_hours'] = df['tfmod'] - df['t0mod']
+
+# convert timedelta to seconds, then hours
+df['expt_time_hours'] = df['expt_time_hours'].map(lambda ts: ts.total_seconds()/60/60)
+
+df.drop(columns=['t0mod', 'tfmod'], inplace=True)
+df.to_csv(csv_file, index=False)
