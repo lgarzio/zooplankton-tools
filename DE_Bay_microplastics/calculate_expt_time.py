e246c1adaa8055fa3106aeb14aa734d35f1e2f1d
lgarzio <lgarzio@marine.rutgers.edu>
2020-03-25 16:27:22 -0400

    consolidated data sheets

--- DE_Bay_microplastics/calculate_expt_time.py
+++ DE_Bay_microplastics/calculate_expt_time.py
@@ -2,16 +2,19 @@
 """
 Created on Jan 30 2020 by Lori Garzio
 @brief Calculate experiment time in hours and add to the input csv file as a column
-csv_file: file containing experiment start and end times
+f: file containing experiment start and end times
+expt: experiment to analyze (options: expt1, expt2)
 """
 
 import datetime as dt
 import pandas as pd
 pd.set_option('display.width', 320, "display.max_columns", 10)  # for display in pycharm console
 
-csv_file = '/Users/lgarzio/Documents/rucool/Saba/microplastics/NOAA2018/data/DEBay_MP_expt2.csv'
+expt = 'expt1'  # expt1 or expt2
+f = ''.join(('/Users/lgarzio/Documents/rucool/Saba/microplastics/NOAA2018/data/DEBay_MP_', expt, '.xlsx'))
+csv_file = ''.join(('/Users/lgarzio/Documents/rucool/Saba/microplastics/NOAA2018/data/DEBay_MP_', expt, '_temp.csv'))
 
-df = pd.read_csv(csv_file)
+df = pd.read_excel(f, sheet_name='expt_data')
 
 df['t0mod'] = df['t0'].map(lambda t: dt.datetime.strptime(t, '%Y-%m-%dT%H:%M'))
 df['tfmod'] = df['tf'].map(lambda t: dt.datetime.strptime(t, '%Y-%m-%dT%H:%M'))
